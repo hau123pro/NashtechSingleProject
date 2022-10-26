2c710d5883a5e865d666879991ef739da1c5a6c4
hau <hau123pro2@gmail.com>
2022-10-26 17:15:30 +0700

    test

--- FE/src/component/showtime.tsx
+++ FE/src/component/showtime.tsx
@@ -1,45 +1,50 @@
-import React,{useState,useEffect, ReactElement} from 'react';
-import {user,movie}  from "../types/type"
+import React, { useState, useEffect, ReactElement } from 'react';
+import { user, movie } from "../types/type"
 import '../assets/bootstrap/css/bootstrap.min.css';
 import testService from '../service/movieService';
 
-const Hello: React.FC<user>= (props:user)  =>{
-    
+const Hello: React.FC<user> = (props: user) => {
+
     // useEffect (() =>{
-        
+
     //     },[])
     const [visible, setVisible] = useState<movie[]>([]);
 
-    useEffect (()=>{
-        const fetchData = async () => {
-            const respone=await testService.getProduct();
-            setVisible(respone.data); 
-            // let obj=new Array('haha','hihi');
-            // setVisible(obj);
-          }
-          fetchData();
-        }
-    ,[]);
-    
-    const createMovieShowTime= () =>{
+    useEffect(() => {
+        // const fetchData = async () => {
+        //     const respone=await testService.getProduct();
+        //     setVisible(respone.data); 
+        //     // let obj=new Array('haha','hihi');
+        //     // setVisible(obj);
+        //   }
+        //   fetchData();
+        testService.getProduct().then(
+            response => {
+                setVisible(response.data);
+            }
+        );
+    }
+        , []);
+
+    const createMovieShowTime = () => {
         console.log("get data");
         // getdata();
-        
-        const movie =new Array();
+
+        const movie = new Array();
         visible.map(
-            item =>{
-                movie.push( <li key={item.movieName} className="list-inline-item"><a className="social-icon text-xs-center" target="_blank" href="#"><img src={item.imgUrl}></img><br/>{item.movieName}</a></li>);
+            item => {
+                movie.push(<li key={item.movieName} className="list-inline-item"><a className="social-icon text-xs-center" target="_blank" href="#"><img src={item.imgUrl}></img><br />{item.movieName}</a></li>);
             }
         )
         return movie;
     }
-    
+
     return (
         <>
-        <div>hello {props.name}</div>
-        <ul className="list-inline">
-            {createMovieShowTime()}
-        </ul>
+            <div>hello {props.name}</div>
+            <ul className="list-inline">
+                {createMovieShowTime()}
+            </ul>
         </>
     )
 }
